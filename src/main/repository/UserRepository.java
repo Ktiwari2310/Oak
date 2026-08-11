09cb6d64e08d5ea4936d3da8f09390bf943ac1da
Ktiwari2310 <beingkt.23@gmail.com>
2026-08-12 00:39:35 +0530

    Done

--- src/main/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oak.repository;
-
-import com.oak.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- src/main/response/UserResponse.java
+++
@@ -1,5 +0,0 @@
-package com.oak.response;
-
-public class UserResponse {
-    
-}
