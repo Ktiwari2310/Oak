c0442e83f8bb1d8e853983cf2dc4843d3312f948
Harshvardhan Singh <harshvardhanrohitassingh@gmail.com>
2026-08-11 21:46:45 +0530

    comment entity

--- src/main/java/com/oak/entity/Comment.java
+++ src/main/java/com/oak/entity/Comment.java
@@ -1,50 +1,62 @@
 package com.oak.entity;
 import jakarta.persistence.*;
-import org.junit.platform.commons.annotation.Testable;
+
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "students")
+@Table(name = "comment")
 public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-    private String email;
-    private String department;
+    private int issue_id;
+    private int poster_id;
+    private String type;
+    private String content;
+    private LocalDateTime created_at = LocalDateTime.now();
 
-    public Student() {
+    public Comment(int issue_id, int poster_id, String type, String content) {
+        this.issue_id = issue_id;
+        this.poster_id = poster_id;
+        this.type = type;
+        this.content = content;
     }
-
-    public Student(String name, String email, String department) {
-        this.name = name;
-        this.email = email;
-        this.department = department;
+    public LocalDateTime getCreated_at() {
+        return created_at;
     }
-
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
     public Long getId() {
         return id;
     }
-    public void setId(Long id){
+    public void setId(Long id) {
         this.id = id;
     }
-    public String getName() {
-        return name;
+    public int getIssue_id() {
+        return issue_id;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setIssue_id(int issue_id) {
+        this.issue_id = issue_id;
     }
-    public String getEmail() {
-        return email;
+    public int getPoster_id() {
+        return poster_id;
     }
-    public void setEmail(String email) {
-        this.email = email;
+    public void setPoster_id(int poster_id) {
+        this.poster_id = poster_id;
     }
-    public String getDepartment() {
-        return department;
+    public String getType() {
+        return type;
     }
-    public void setDepartment(String department) {
-        this.department = department;
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
     }
 }
