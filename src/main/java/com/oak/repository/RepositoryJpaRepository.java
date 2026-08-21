56e08da339b1783e050aa1512fb8bfc45e8150b9
Khushboo <beingkt.23@gmail.com>
2026-08-21 09:52:49 +0530

    Delete src/main/java/com/oak/repository/RepositoryJpaRepository.java

--- src/main/java/com/oak/repository/RepositoryJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oak.repository;
-
-import com.oak.entity.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositoryJpaRepository extends JpaRepository<Repository, Long> {
-}
